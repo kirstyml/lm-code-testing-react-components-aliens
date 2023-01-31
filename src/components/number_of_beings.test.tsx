dbb34c5c548a451ed345fa275162a9a47ca4d921
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-01-31 16:33:52 +0000

    updated test descriptions for mnumberOfBeings

--- src/components/number_of_beings.test.tsx
+++ src/components/number_of_beings.test.tsx
@@ -1,21 +1,27 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event'
-import NumberOfBeings, {NumberOfBeingsProps} from './number_of_beings';
+import NumberOfBeings, { NumberOfBeingsProps } from './number_of_beings';
 
 describe("NumberOfBeings", () => {
-    it("renders NumberOfBeings input", () => {
-        render(<NumberOfBeings numberOfBeings='' onChangeNumberOfBeings={() => {}} />);
+    it(`Given the required props, 
+        When the component renders, 
+        the NumberOfBeings label should be present`, () => {
+        render(<NumberOfBeings numberOfBeings='' onChangeNumberOfBeings={() => { }} />);
         const labelText = screen.getByText("Number of beings:");
         expect(labelText).toBeInTheDocument();
     });
-    it("displays the input value", () => {
-        render(<NumberOfBeings numberOfBeings='600' onChangeNumberOfBeings={() => {}} />);
+    it(`Given the value of number of beings,
+        When the component renders, 
+        it should displays the input value`, () => {
+        render(<NumberOfBeings numberOfBeings='600' onChangeNumberOfBeings={() => { }} />);
         const inputValue = screen.getByDisplayValue('600');
         expect(inputValue).toBeInTheDocument();
     });
-    it("calls the onChange function when it is updated",async () => {
+    it(`Given the input has rendered, 
+        when a user types in the input, 
+        then the onChange function is called and returns the typed value`, async () => {
         const mockInputChange = jest.fn(e => e.target.value);
-        const numberOfBeingsProps : NumberOfBeingsProps = {
+        const numberOfBeingsProps: NumberOfBeingsProps = {
             numberOfBeings: '',
             onChangeNumberOfBeings: mockInputChange
         }
