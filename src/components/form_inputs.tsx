5abdc8a599f55965eec13166dbde50081ebae006
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-01-31 13:05:40 +0000

    Added console.log of form data on submit

--- src/components/form_inputs.tsx
+++ src/components/form_inputs.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import SpeciesName from "./species_name";
 import PlanetName from "./planet_name";
 import NumberOfBeings from "./number_of_beings";
@@ -12,8 +12,17 @@ const FormInputs : React.FC = () => {
     const [sumAnswer, setSumAnswer] = useState('Not 4');
     const [reasonForSparing, setReasonForSparing] = useState('');
 
+    const handleSubmit = (event : React.FormEvent<HTMLFormElement>) => {
+        console.log(`speciesName: ${speciesName}`);
+        console.log(`planetName: ${planetName}`);
+        console.log(`numberOfBeings: ${numberOfBeings}`);
+        console.log(`sumAnswer: ${sumAnswer}`);
+        console.log(`reasonForSparing: ${reasonForSparing}`);
+        event.preventDefault();
+    };
+
     return (
-        <form action="">
+        <form onSubmit={(event) => handleSubmit(event)}>
             <SpeciesName speciesName={speciesName} setSpeciesName={setSpeciesName}  />
             <PlanetName planetName={planetName} setPlanetName={setPlanetName} />
             <NumberOfBeings numberOfBeings={numberOfBeings} setNumberOfBeings={setNumberOfBeings} />
