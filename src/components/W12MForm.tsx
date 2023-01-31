fcd1b76ee2088ba96757fcfb9986fa0255861dac
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-01-31 14:43:03 +0000

    Moved forminputs into W12MForms and added SubmittedInfo

--- src/components/W12MForm.tsx
+++ src/components/W12MForm.tsx
@@ -1,12 +1,59 @@
 import { useState } from 'react';
 import W12MHeader from './W12MHeader';
-import FormInputs from './form_inputs';
+import SpeciesName from "./species_name";
+import PlanetName from "./planet_name";
+import NumberOfBeings from "./number_of_beings";
+import SumAnswer from "./sum_answer";
+import ReasonForSparing from "./reason_for_sparing";
+import SubmitButton from "./submit_button";
+import SubmittedInfo from './submitted_info';
 
 const W12MForm = () => {
+	const [speciesName, setSpeciesName] = useState('');
+	const [planetName, setPlanetName] = useState('');
+	const [numberOfBeings, setNumberOfBeings] = useState('');
+	const [sumAnswer, setSumAnswer] = useState('Not 4');
+	const [reasonForSparing, setReasonForSparing] = useState('');
+	const [submitted, setSubmitted] = useState(false);
+
+	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		console.log(`speciesName: ${speciesName}`);
+		console.log(`planetName: ${planetName}`);
+		console.log(`numberOfBeings: ${numberOfBeings}`);
+		console.log(`sumAnswer: ${sumAnswer}`);
+		console.log(`reasonForSparing: ${reasonForSparing}`);
+		setSubmitted(true);
+		event.preventDefault();
+	};
 	return (
 		<section className='w12MForm'>
 			<W12MHeader />
-			<FormInputs />
+			<form onSubmit={(event) => handleSubmit(event)}>
+				<SpeciesName
+					speciesName={speciesName}
+					onChangeSpeciesName={(event) => setSpeciesName(event.target.value)} />
+				<PlanetName
+					planetName={planetName}
+					onChangePlanetName={(event) => setPlanetName(event.target.value)} />
+				<NumberOfBeings
+					numberOfBeings={numberOfBeings}
+					onChangeNumberOfBeings={(event) => setNumberOfBeings(event.target.value)} />
+				<SumAnswer
+					sumAnswer={sumAnswer}
+					onChangeSumAnswer={(event) => setSumAnswer(event.target.value)} />
+				<ReasonForSparing
+					reasonForSparing={reasonForSparing}
+					onChangeReasonForSparing={(event) => setReasonForSparing(event.target.value)} />
+				<SubmitButton />
+			</form>
+			<SubmittedInfo
+				speciesName={speciesName}
+				planetName={planetName}
+				numberOfBeings={numberOfBeings}
+				sumAnswer={sumAnswer}
+				reasonForSparing={reasonForSparing}
+				submitted={submitted}
+			/>
 		</section>
 	);
 };
---
+++ src/components/submitted_info.tsx
@@ -0,0 +1,38 @@
+import NumberOfBeings from "./number_of_beings";
+import SpeciesName from "./species_name";
+
+interface SubmittedInfoProps {
+    speciesName: string,
+    planetName: string,
+    numberOfBeings: string,
+    sumAnswer: string,
+    reasonForSparing: string,
+    submitted: boolean
+}
+
+const SubmittedInfo : React.FC<SubmittedInfoProps> = ({ 
+    speciesName, 
+    planetName,
+    numberOfBeings,
+    sumAnswer,
+    reasonForSparing, 
+    submitted
+ }) => {
+    return (
+        <section>
+            <h2>Submitted Information:</h2>
+            {
+                submitted && 
+                <>
+                    <p>Species: {speciesName}</p>
+                    <p>Planet: {planetName}</p>
+                    <p>Number of beings: {numberOfBeings}</p>
+                    <p>What is 2 + 2: {sumAnswer}</p>
+                    <p>Reason for sparing: {reasonForSparing}</p>
+                </>   
+            }
+        </section>
+    )
+}
+
+export default SubmittedInfo;
