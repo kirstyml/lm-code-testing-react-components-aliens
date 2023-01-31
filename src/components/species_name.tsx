b40cbcca6ef052ca9a1a4e37cd4d277e20abd156
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-01-31 12:35:27 +0000

    formatting correction

--- src/components/species_name.tsx
+++ src/components/species_name.tsx
@@ -3,11 +3,16 @@ interface SpeciesNameProps {
     setSpeciesName: (name: string) => void
 }
 
-const SpeciesName : React.FC<SpeciesNameProps> = ({ speciesName, setSpeciesName }) => {
+const SpeciesName: React.FC<SpeciesNameProps> = ({ speciesName, setSpeciesName }) => {
     return (
         <>
             <label htmlFor="speciesName">Species Name: </label>
-            <input id="speciesName" type="text" value={speciesName} onChange={(event) => {setSpeciesName(event.target.value)}} />
+            <input
+                id="speciesName"
+                type="text"
+                value={speciesName}
+                onChange={(event) => { setSpeciesName(event.target.value) }}
+            />
         </>
     )
 }
