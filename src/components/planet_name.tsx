35590eae852ec78f66363257c0b20619ec24a149
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-02-02 09:28:16 +0000

    Added validation to planetName

--- src/components/planet_name.tsx
+++ src/components/planet_name.tsx
@@ -1,9 +1,23 @@
+import React, { useState } from "react";
+import ErrorMessage from "./error_message";
+
 export interface PlanetNameProps {
     planetName: string,
     onChangePlanetName: (event : React.ChangeEvent<HTMLInputElement>) => void
 }
 
 const PlanetName: React.FC<PlanetNameProps> = ({ planetName, onChangePlanetName }) => {
+    const [errorMessage, setErrorMessage] = useState<string | undefined>();
+
+    const validate = (value : string) : string | undefined => {
+        if(value.length < 2 || value.length > 49) {
+            return "Planet name must be between 2 and 49 characters"
+        }
+        if(!(/^[a-zA-Z0-9]*$/).test(value)) {
+            return "Planet name cannot contain special characters"
+        }
+    };
+
     return (
         <>
             <label htmlFor="planetName">Planet Name: </label>
@@ -11,8 +25,13 @@ const PlanetName: React.FC<PlanetNameProps> = ({ planetName, onChangePlanetName 
                 id="planetName" 
                 type="text" 
                 value={planetName} 
-                onChange={onChangePlanetName}
+                onChange={(e) => {
+                    const errorMessage = validate(e.target.value);
+                    setErrorMessage(errorMessage);
+                    onChangePlanetName(e);
+                }}
             />
+            {errorMessage && <ErrorMessage errorMessage={errorMessage} />}
         </>
 
     )
