cd9f5ec2679b5497fc0b9e565bee63d19ae76f4c
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-02-02 09:38:43 +0000

    Added validation to sumAnswer

--- src/components/sum_answer.tsx
+++ src/components/sum_answer.tsx
@@ -1,22 +1,37 @@
+import React, { useState } from "react";
+import ErrorMessage from "./error_message";
+
 export interface SumAnswerProps {
     sumAnswer: string,
     onChangeSumAnswer: (event : React.ChangeEvent<HTMLSelectElement>) => void
 }
 
 const SumAnswer: React.FC<SumAnswerProps> = ({ sumAnswer, onChangeSumAnswer }) => {
+    const [errorMessage, setErrorMessage] = useState<string | undefined>();
+
+    const validate = (value : string) : string | undefined => {
+        if(value === "Not 4") {
+            return "Incorrect answer."
+        }
+    };
+
     return (
         <>
             <label htmlFor="sumAnswer">What is 2 + 2? </label>
             <select 
                 id="sumAnswer" 
                 value={sumAnswer} 
-                onChange={onChangeSumAnswer}
+                onChange={(e) => {
+                    const errorMessage = validate(e.target.value);
+                    setErrorMessage(errorMessage);
+                    onChangeSumAnswer(e);
+                }}
             >
                 <option value="4">4</option>
                 <option value="Not 4">Not 4</option>
             </select>
+            {errorMessage && <ErrorMessage errorMessage={errorMessage} />}
         </>
-
     )
 }
 
