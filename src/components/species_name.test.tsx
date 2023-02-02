ed519aec950a22dee7d8036cf7497979acd47644
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-02-02 13:16:08 +0000

    Added validation tests to speciesName

--- src/components/species_name.test.tsx
+++ src/components/species_name.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event'
 import SpeciesName, { SpeciesNameProps } from './species_name';
 
@@ -31,5 +31,85 @@ describe("SpeciesName", () => {
         await userEvent.type(input, 'Human');
         expect(mockInputChange).toHaveBeenCalledTimes(5);
         expect(mockInputChange).toHaveLastReturnedWith("n");
+    });
+    it(`Given the input has rendered, 
+        when a valid species name is entered, 
+        then there is no error message present`, async () => {
+        const mockInputChange = jest.fn(e => e.target.value);
+        const SpeciesNameProps: SpeciesNameProps = {
+            speciesName: '',
+            onChangeSpeciesName: mockInputChange
+        }
+        render(<SpeciesName {...SpeciesNameProps} />);
+        const input = screen.getByRole('textbox');
+        expect(input).toBeInTheDocument();
+        fireEvent.change(input, { target: { value: 'Human' } });
+        expect(
+            screen.queryByText('Species name ', { exact: false })
+        ).not.toBeInTheDocument();
+    });
+    it(`Given the input has rendered, 
+        when a species name that is too short is entered, 
+        then there is no error message present`, async () => {
+        const mockInputChange = jest.fn(e => e.target.value);
+        const SpeciesNameProps: SpeciesNameProps = {
+            speciesName: '',
+            onChangeSpeciesName: mockInputChange
+        }
+        render(<SpeciesName {...SpeciesNameProps} />);
+        const input = screen.getByRole('textbox');
+        expect(input).toBeInTheDocument();
+        fireEvent.change(input, { target: { value: 'Hu' } });
+        expect(
+            screen.getByText('Species name must be between', { exact: false })
+        ).toBeInTheDocument();
+    });
+    it(`Given the input has rendered, 
+        when a species name that is too long is entered, 
+        then there is no error message present`, async () => {
+        const mockInputChange = jest.fn(e => e.target.value);
+        const SpeciesNameProps: SpeciesNameProps = {
+            speciesName: '',
+            onChangeSpeciesName: mockInputChange
+        }
+        render(<SpeciesName {...SpeciesNameProps} />);
+        const input = screen.getByRole('textbox');
+        expect(input).toBeInTheDocument();
+        fireEvent.change(input, { target: { value: 'qwertyuiopasdfghjklzxcvbnm' } });
+        expect(
+            screen.getByText('Species name must be between', { exact: false })
+        ).toBeInTheDocument();
+    });
+    it(`Given the input has rendered, 
+        when an invalid species name containing a number is entered, 
+        then there is no error message present`, async () => {
+        const mockInputChange = jest.fn(e => e.target.value);
+        const SpeciesNameProps: SpeciesNameProps = {
+            speciesName: '',
+            onChangeSpeciesName: mockInputChange
+        }
+        render(<SpeciesName {...SpeciesNameProps} />);
+        const input = screen.getByRole('textbox');
+        expect(input).toBeInTheDocument();
+        fireEvent.change(input, { target: { value: 'Human5' } });
+        expect(
+            screen.getByText('Species name cannot contain a', { exact: false })
+        ).toBeInTheDocument();
+    });
+    it(`Given the input has rendered, 
+        when an invalid species name containing a special character is entered, 
+        then there is no error message present`, async () => {
+        const mockInputChange = jest.fn(e => e.target.value);
+        const SpeciesNameProps: SpeciesNameProps = {
+            speciesName: '',
+            onChangeSpeciesName: mockInputChange
+        }
+        render(<SpeciesName {...SpeciesNameProps} />);
+        const input = screen.getByRole('textbox');
+        expect(input).toBeInTheDocument();
+        fireEvent.change(input, { target: { value: 'H!2' } });
+        expect(
+            screen.getByText('Species name cannot contain a', { exact: false })
+        ).toBeInTheDocument();
     });
 })
