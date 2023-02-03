86734fdc1c945d912bbaf9e77d22f6ef49926b6a
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-02-03 09:37:23 +0000

    Created validate function for text input

--- src/components/text_input.tsx
+++ src/components/text_input.tsx
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
 import ErrorMessage from "./error_message";
+import { validate } from "./validate";
 
 export interface TextInputProps {
     name: string,
     label: string,
     value: string,
     onChangeTextInput: (event : React.ChangeEvent<HTMLInputElement>) => void,
-    validate: (value: string) => string | undefined
 }
 
-const TextInput: React.FC<TextInputProps> = ({ name, label, value, onChangeTextInput, validate }) => {
+const TextInput: React.FC<TextInputProps> = ({ name, label, value, onChangeTextInput }) => {
     const [errorMessage, setErrorMessage] = useState<string | undefined>();
 
     return (
@@ -21,7 +21,7 @@ const TextInput: React.FC<TextInputProps> = ({ name, label, value, onChangeTextI
                 type="text"
                 value={value}
                 onChange={(e) => {
-                    const errorMessage = validate(e.target.value);
+                    const errorMessage = validate({value: e.target.value, type: name});
                     setErrorMessage(errorMessage);
                     onChangeTextInput(e);
                 }}
